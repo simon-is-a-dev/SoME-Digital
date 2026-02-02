43f8aee251131a9bdd572e57e5fa6401db0f76eb
Simon Swart <simon@nevadacloud.com>
2026-02-02 16:31:22 +0200

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// app/page.tsx  (or pages/index.tsx)
-import Layout from "@/components/Layout";
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
 import Services from "@/components/sections/Services";
@@ -8,12 +6,13 @@ import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <Hero />
       <About />
       <Services />
       <Features />
       <Contact />
-    </Layout>
+    </>
   );
 }
+
